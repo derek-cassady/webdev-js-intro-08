594e536c53bdc3a95b2c7eaadb4da88c6330fde5
derek-cassady <derek@cassadyweb.com>
2025-01-12 15:19:04 -0500

    did experiment, learned something new

--- js/index.js
+++ js/index.js
@@ -1,3 +1,40 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+// Define an array of numbers
+const numbers = [10, 20, 30, 40, 50];
+
+// Use a basic for loop
+// for (let i = 0; i < numbers.length; i++) {
+//     console.log(numbers[i]); 
+//     // Log each number
+// }
+
+// // Use 'for...of' to loop
+// for (const number of numbers) {
+//     console.log(number); 
+//     // Logs each number
+// }
+
+// // Use 'forEach' to loop
+// numbers.forEach(function(number) {
+//     console.log(number); 
+//     // Logs each number
+// });
+
+// // Use 'for...of' to assign new values
+// for (let number of numbers) {
+//     number += 2; 
+//     // This does NOT modify the original array
+// }
+// console.log(numbers); 
+// // [ 10, 20, 30, 40, 50 ] (unchanged)
+
+// Use 'forEach' to assign new values
+numbers.forEach(function(number, index, array) {
+    array[index] = number + 2; 
+    // Modifies the original array
+});
+console.log(numbers); 
+// [ 12, 22, 32, 42, 52 ]
